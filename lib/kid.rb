701e4773e8e9f24bebeea7605bf3e710485f65de
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-23 21:48:19 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,15 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  # include Dance
-  include FancyDance::InstanceMethods
-  # extend MetaDancing
   extend FancyDance::ClassMethods
- 
+  include FancyDance::InstanceMethods
+
   attr_accessor :name
- 
+
   def initialize(name)
-    @name = name
-  end
+    @nam
   
 end
